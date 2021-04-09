ea0e81114e5b66e316992e8938b3a31bfacd6bd6
TomNin97 <chm16@zips.uakron.edu>
2021-04-09 18:33:17 -0400

    Created foundations for 2d, 3d, multi, and main pages

---
+++ 3d/cube.js
@@ -0,0 +1,10 @@
+canvas = document.getElementById( "gl-canvas" );
+
+var gl = canvas.getContext("webgl")
+|| canvas.getContext("experimental-webgl");
+if ( !gl ) { alert( "WebGL isn't available" ); }
+
+gl.viewport(0, 0,
+gl.drawingBufferWidth, gl.drawingBufferHeight);
+gl.clearColor(0.0, 0.5, 0.0, 0.25);
+gl.clear(gl.COLOR_BUFFER_BIT);
