b685a44e2faf1b09f430adc4382cac8918640e6f
Faith Sekerak <59906879+fmsekerak@users.noreply.github.com>
2021-04-18 21:36:36 -0400

    Triangle Work

--- 2d/triangle.js
+++ 2d/triangle.js
@@ -1,10 +1,42 @@
-canvas = document.getElementById( "triangle-canvas" );
+var gl;
+var points;
 
-var gl = canvas.getContext("webgl")
-|| canvas.getContext("experimental-webgl");
-if ( !gl ) { alert( "WebGL isn't available" ); }
+window.onload = function init(){
+    var canvas = document.getElementById( "triangle-canvas" );
+    gl = canvas.getContext("webgl")
+    || canvas.getContext("experimental-webgl");
+    if ( !gl ) { alert( "WebGL isn't available" ); }
 
-gl.viewport(0, 0,
-gl.drawingBufferWidth, gl.drawingBufferHeight);
-gl.clearColor(0.0, 0.5, 0.0, 0.25);
-gl.clear(gl.COLOR_BUFFER_BIT);
+    var vertices = [
+        vec2( -0.5, -0.5 ),
+        vec2(  0,  0.5 ),
+        vec2(  0.5, -0.5 )    
+        ];    
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor(1.0, 1.0, 1.0, 1.0);
+
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 2, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+    render();
+};
+
+function render() {
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    gl.drawArrays( gl.TRIANGLES, 0, 3 );
+}
+
+
+
+
+
+
+
